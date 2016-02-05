4f3b00a9c769327b7195bdafdb9ac01e8a008e00
kappataumu <hello@kappataumu.com>
2016-02-05 00:16:28 +0000

    Be explicit about _site location. Group comments

--- bootstrap.sh
+++ bootstrap.sh
@@ -61,19 +61,29 @@ if [[ ! -d "$clonedir" ]]; then
     git clone "$clonerepo" "$clonedir"
 fi
 
-# Now, for the Jekyll part. Due to jekyll/jekyll#3030 we need to  
-# detach Jekyll from the shell manually, if we want --watch to work.
+
+# Now, for the Jekyll part. There are some issues you might hit:
+#
+# * Due to jekyll/jekyll#3030 we need to detach Jekyll from the shell manually,
+#   if we want --watch to work.
+#
+# * We need Vagrant >= 1.8 to fix a regression that botched emission of the
+#   vagrant-mounted upstart event, see mitchellh/vagrant#6074 for details.
+#
+# * We need Ruby 2.1.7p400 due to what appears to be a regression in Ruby's
+#   FileUtils core module, see http://stackoverflow.com/q/33091988
+
 jekyll=$(which jekyll)
 wrapper="${jekyll/bin/wrappers}"
 log="/home/vagrant/jekyll.log"
-run="nohup $wrapper serve --source $clonedir --watch --force_polling >> $log 2>&1 &"
+run="nohup $wrapper serve --source $clonedir --destination $clonedir --watch --force_polling >> $log 2>&1 &"
 eval $run
 
 cat << UPSTART | sudo tee /etc/init/jekyll.conf > /dev/null
 description "Jekyll"
 author "kappataumu <hello@kappataumu.com>"
 
-# You need Vagrant >= 1.8 to fix a regression that botched emission of this 
+# You need Vagrant >= 1.8 to fix a regression that botched emission of this
 # upstart event, see mitchellh/vagrant#6074 for details.
 start on vagrant-mounted MOUNTPOINT=/srv/www
 
